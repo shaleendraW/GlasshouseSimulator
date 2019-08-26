988a2a6a71a0ccee071c99cbe8773429494dd051
shaleendraW <tshaleendra9@gmail.com>
2019-08-26 23:45:19 +0530

    add slider and text value
    Create script to get value form slider
    call showvalue

---
+++ PhotoTemplate2/Assets/Humidity/MyScripts/ShowvalueScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowvalueScript : MonoBehaviour
+{
+
+
+    Text celciusText;
+
+    void Start()
+    {
+        celciusText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+   public void textUpdate( float value)
+    {
+        celciusText.text = Mathf.RoundToInt(value)+25+"°c";
+    }
+}
