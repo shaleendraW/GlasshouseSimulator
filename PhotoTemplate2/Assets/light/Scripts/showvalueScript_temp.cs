ae78ae7bcb7335c09f852287bba7b7b69e983f32
shaleendraW <tshaleendra9@gmail.com>
2019-08-29 02:28:13 +0530

    create light scene
    1.create back ,simulate ,and other button
    2.add sensor object to prefab
    3.create slider and text as well

---
+++ PhotoTemplate2/Assets/light/Scripts/showvalueScript_temp.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Experimental.XR;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.EventSystems;
+
+public class showvalueScript_temp : MonoBehaviour
+{
+
+    
+    public static bool disabled = false;
+    public int slidervalue;
+
+
+    Text celciusText;
+
+    void Start()
+    {
+        celciusText = GetComponent<Text>();
+
+    }
+    // Update is called once per frame
+    public void textUpdate(float value)
+    {
+        slidervalue = Mathf.RoundToInt(value) + 25;
+        celciusText.text = slidervalue + "°c";
+    }
+    void Update()
+    {
+        print(slidervalue);
+        if (slidervalue == 30)
+        {
+            
+            
+           
+
+        }
+        else if (slidervalue == 15)
+        {
+           
+        }
+        else
+        {
+           
+        }
+
+    }
+
+
+
+}
