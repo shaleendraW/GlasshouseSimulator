3ca0785940e8d662c73e1cac2ef48cc3a240aa2e
Nipun Deshaka <30893324+Nipun96@users.noreply.github.com>
2019-08-28 19:40:28 +0530

    using slider chnage fan

---
+++ PhotoTemplate2/Assets/Humidity/MyScripts/sliderChange.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class sliderChange : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float RHslider;
+    Text humidtydisplay;
+    void Start()
+    {
+        humidtydisplay = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    public void sliderobj(float speed)
+    {
+        RHslider = speed;
+        humidtydisplay.text = speed + "RH%";
+       
+    }
+    void Update()
+    {
+
+        if (RHslider > 80)
+        {
+            fanRotate.rotatebool = true;
+            fanRotate.speed = -1500.0f;
+
+        }
+        else if(RHslider > 70)
+        {
+            fanRotate.rotatebool = true;
+            fanRotate.speed = -1000.0f;
+        }
+        else if (RHslider > 60)
+        {
+            fanRotate.rotatebool = true;
+            fanRotate.speed = -500.0f;
+        }
+        else
+        {
+            fanRotate.rotatebool = false;
+        }
+    }
+}
