0deb12aa37f7cf0910dc797e2eb47de8384c9364
Nipun Deshaka <30893324+Nipun96@users.noreply.github.com>
2019-08-30 12:25:27 +0530

    Update enableSmoke.cs

--- PhotoTemplate2/Assets/Humidity/MyScripts/enableSmoke.cs
+++ PhotoTemplate2/Assets/Humidity/MyScripts/enableSmoke.cs
@@ -19,5 +19,8 @@ public class enableSmoke : MonoBehaviour
     {
        if(enablesmoke)
         smoke.gameObject.SetActive(true);
+       else
+            smoke.gameObject.SetActive(false);
+
     }
 }
