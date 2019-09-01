a0e14867a00db3e879f82ae0cc081b304efe0f8c
Nipun Deshaka <30893324+Nipun96@users.noreply.github.com>
2019-09-01 19:17:48 +0530

    update new fan

---
+++ PhotoTemplate2/Assets/Humidity/MyScripts/huRotateFan.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class huRotateFan : MonoBehaviour
+{
+    public float spinspeed = -0.0050f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(spinspeed * Time.deltaTime, 0 , spinspeed * Time.deltaTime);
+    }
+}
