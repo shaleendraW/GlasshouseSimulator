e2332c3eea80e74b2a7fb4b20696f0d3c94aae5d
Nipun Deshaka <30893324+Nipun96@users.noreply.github.com>
2019-09-03 00:48:40 +0530

    change main interface

---
+++ PhotoTemplate2/Assets/Soil/Script/simulate_menue3.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class simulate_menue3 : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public void changeScene(string sceneName)
+    {
+        Application.LoadLevel(sceneName);
+    }
+
+}
