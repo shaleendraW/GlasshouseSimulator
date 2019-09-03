290f917343918138915e8555341655499be98420
shaleendraW <tshaleendra9@gmail.com>
2019-09-03 07:01:53 +0530

    add colour pallet to light function

---
+++ PhotoTemplate2/Assets/light/Scripts/Activate_colour_pallet_light.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activate_colour_pallet_light : MonoBehaviour
+{
+    public GameObject Colorpaletlight;
+    public static bool activatelightcolourpallet = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (activatelightcolourpallet)
+
+            Colorpaletlight.SetActive(true);
+
+        else
+
+            Colorpaletlight.SetActive(false);
+            
+        
+
+    }
+}
